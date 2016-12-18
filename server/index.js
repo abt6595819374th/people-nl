b6d8910cbd5095b7ad1a9ccbb5eb5ab1d4978928
abt6595819374th <abt6595819374th@gmail.com>
2016-12-18 14:45:40 +0100

    Front end app bootstraps on top of server-generated content

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
+var path = require('path');
 
 app.use(require('./controllers'));
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({defaultLayout: path.join(__dirname, '/views/layouts/main')}));
 app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, '/views'));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
